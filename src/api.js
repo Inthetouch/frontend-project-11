451d4c2140e0f734456800795ebac41d6ea73a42
Inthetouch <nikita_eremeev@mail.ru>
2025-10-13 00:12:27 +0300

    add api.js and parser.js

---
+++ src/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const buildProxyUrl = (url) => {
+  const proxyUrl = new URL('https://allorigins.hexlet.app/get')
+  proxyUrl.searchParams.set('disableCache', 'true')
+  proxyUrl.searchParams.set('url', url)
+  return proxyUrl.toString()
+}
+
+export default (url) => {
+  const fullUrl = buildProxyUrl(url)
+
+  return axios.get(fullUrl)
+    .then((response) => {
+      return response.data.contents
+    })
+    .catch((error) => {
+      throw error
+    })
+}
---
+++ src/parser.js
@@ -0,0 +1,26 @@
+export default (rssString) => {
+  const parser = new DOMParser()
+  const xmlDoc = parser.parseFromString(rssString, 'application/xml')
+
+  const parseError = xmlDoc.querySelector('parseerror')
+  if (parseError) {
+    throw new Error(parseError.textContent)
+  }
+
+  const feedTitle = xmlDoc.querySelector('channel title').textContent
+  const feedDescription = xmlDoc.querySelector('channel description').textContent
+  const itemElements = xmlDoc.querySelectorAll('item')
+
+  const posts = Array.from(itemElements).map((itemElement) => {
+    const postTitle = itemElement.querySelector('title').textContent
+    const postLink = itemElement.querySelector('link').textContent
+
+    return { title: postTitle, link: postLink }
+  })
+
+  return {
+    title: feedTitle,
+    description: feedDescription,
+    posts,
+  }
+}

578a4b669fb9028778b33473b7abba6d6ff7083f
Inthetouch <nikita_eremeev@mail.ru>
2025-10-20 00:47:31 +0300

    fix parser

--- src/parser.js
+++ src/parser.js
@@ -2,25 +2,31 @@ export default (rssString) => {
   const parser = new DOMParser()
   const xmlDoc = parser.parseFromString(rssString, 'application/xml')
 
-  const parseError = xmlDoc.querySelector('parseerror')
+  const parseError = xmlDoc.querySelector('parsererror')
   if (parseError) {
-    throw new Error(parseError)
+    throw new Error('parseError')
   }
 
-  const feedTitle = xmlDoc.querySelector('channel title').textContent
-  const feedDescription = xmlDoc.querySelector('channel description').textContent
-  const itemElements = xmlDoc.querySelectorAll('item')
+  try {
+    const feedTitle = xmlDoc.querySelector('channel title').textContent
+    const feedDescription = xmlDoc.querySelector('channel description').textContent
 
-  const posts = Array.from(itemElements).map((itemElement) => {
-    const postTitle = itemElement.querySelector('title').textContent
-    const postLink = itemElement.querySelector('link').textContent
-    const postDescription = itemElement.querySelector('description').textContent
-    return { title: postTitle, link: postLink, description: postDescription }
-  })
+    const itemElements = xmlDoc.querySelectorAll('item')
+    const posts = Array.from(itemElements).map((item) => {
+      const postTitle = item.querySelector('title').textContent
+      const postLink = item.querySelector('link').textContent
+      const postDescription = item.querySelector('description').textContent
+      return { title: postTitle, link: postLink, description: postDescription }
+    })
 
-  return {
-    title: feedTitle,
-    description: feedDescription,
-    posts,
+    return {
+      title: feedTitle,
+      description: feedDescription,
+      posts,
+    }
+  }
+  catch (e) {
+    console.log(e)
+    throw new Error('parseError')
   }
 }
